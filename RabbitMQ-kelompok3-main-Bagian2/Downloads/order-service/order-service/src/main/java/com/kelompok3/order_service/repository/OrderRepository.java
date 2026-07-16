1438120667831227afccad6aa254de843a2f695e
Michael Julio Sonda <juliosonda51@gmail.com>
2026-07-16 11:05:07 +0700

    Delete RabbitMQ-kelompok3-main-Bagian2/Downloads/order-service/order-service directory

--- RabbitMQ-kelompok3-main-Bagian2/Downloads/order-service/order-service/src/main/java/com/kelompok3/order_service/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kelompok3.order_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kelompok3.order_service.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
